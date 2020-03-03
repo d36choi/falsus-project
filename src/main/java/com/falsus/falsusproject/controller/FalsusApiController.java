cb75f8d54c39bcb9359a6514fc74f980b0c364b0
d36choi <40600306+d36choi@users.noreply.github.com>
2020-03-03 21:54:22 +0900

    Add json data controller

--- src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
+++ src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
@@ -1,12 +1,22 @@
 package com.falsus.falsusproject.controller;
 
 import com.falsus.falsusproject.dao.FalsusRepository;
+import com.falsus.falsusproject.model.Falsus;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/falsus")
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Controller
 public class FalsusApiController {
 
     @Autowired
@@ -23,4 +33,15 @@ public class FalsusApiController {
         model.addAttribute("falsuses", falsusRepository.findAll());
         return "falsus-map";
     }
+
+    @ResponseBody
+    @GetMapping("/json/data")
+    public List<Falsus> GetJsonFalsus(){
+
+    List<Falsus> falsusList= new ArrayList<>();
+    falsusRepository.findAll().forEach(falsusList::add);
+    log.info(falsusList.toString());
+    return falsusList;
+    }
+
 }
