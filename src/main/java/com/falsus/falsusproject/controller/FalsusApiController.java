2caf42ba3b797d359b0bfb7183dab5d1225295cb
mona <kjwook7522@gmail.com>
2020-03-03 19:25:08 +0900

    error fix

--- src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
+++ src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
@@ -1,16 +1,15 @@
 package com.falsus.falsusproject.controller;
 
 import com.falsus.falsusproject.dao.FalsusRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@AllArgsConstructor
 @Controller("/falsus")
 public class FalsusApiController {
 
+    @Autowired
     private FalsusRepository falsusRepository;
 
     @GetMapping("/list")
