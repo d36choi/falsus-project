eb867c4e0b2d33fbcdeceb1ef7bc5ebbebe66279
d36choi <40600306+d36choi@users.noreply.github.com>
2020-03-03 17:09:09 +0900

    Add geolocation column in Entity & update url

--- src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
+++ src/main/java/com/falsus/falsusproject/controller/FalsusApiController.java
@@ -1,15 +1,16 @@
 package com.falsus.falsusproject.controller;
 
 import com.falsus.falsusproject.dao.FalsusRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/fasus")
+@AllArgsConstructor
+@Controller("/falsus")
 public class FalsusApiController {
 
-    @Autowired
     private FalsusRepository falsusRepository;
 
     @GetMapping("/list")
